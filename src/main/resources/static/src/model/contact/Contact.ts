a96691dfe19e36daa7bf5010ed8391296d7e13c9
James G Willmore <jim@localhost.localdomain>
2023-01-06 09:14:44 -0500

    Apply Spotless.

--- src/main/resources/static/src/model/contact/Contact.ts
+++ src/main/resources/static/src/model/contact/Contact.ts
@@ -1,7 +1,5 @@
 /* James G Willmore - LJ Computing - (C) 2023 */
 import Model from "./Model";
-
-/* James G Willmore - LJ Computing - (C) 2022 */
 interface Contact extends Model {
     givenName?: string;
     middleName?: string;
